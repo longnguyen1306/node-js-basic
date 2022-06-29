273bcced8bca3cde86ccd0b774e75f0a4e81919b
LongNguyen <ldc.longnd@gmail.com>
2022-06-29 15:05:13 +0700

    routers & mvc

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 require('dotenv').config();
+import initWebRoute from './route/web'
 
 const app = express()
 const port = process.env.PORT || 8080
 
 configViewEngine(app);
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log('Server Express Running On Port: ', port)
