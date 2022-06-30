b723b2023305c697466227b52ac37114f993b5bf
LongNguyen <ldc.longnd@gmail.com>
2022-06-30 14:39:14 +0700

    form

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,12 +7,40 @@ let getHomePage = async (req, res) => {
 
 let getDetailUser = async (req, res) => {
     let id = req.params.id;
-
     let [user] = await pool.execute('SELECT * FROM users WHERE id = ?',[id] )
     return res.send(JSON.stringify(user));
 }
 
+let createNewUser = async (req, res) => {
+    let {fname, lname, email, address} = req.body;
+    await pool.execute('INSERT INTO users (first_name, last_name, email, address) VALUES (?, ?, ?, ?)', [fname, lname, email, address])
+    return res.redirect('/');
+}
+
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('DELETE FROM users WHERE id = ?', [userId]);
+    return res.redirect('/');
+}
+
+let editUser = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('SELECT * FROM users WHERE id = ?',[id] )
+    return res.render('update', {user: user[0]});
+}
+
+let updatetUser = async (req, res) => {
+    let {fname, lname, email, address, id} = req.body;
+    await pool.execute('UPDATE users SET first_name=?, last_name=?, email=?, address=? WHERE id=?', [fname, lname, email, address, id])
+    console.log(req.body)
+    return res.redirect('/');
+}
+
 module.exports = {
     getHomePage,
-    getDetailUser
+    getDetailUser,
+    createNewUser,
+    deleteUser,
+    editUser,
+    updatetUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,10 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/user/:id', homeController.getDetailUser);
+    router.post('/create-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.editUser);
+    router.post('/update-user', homeController.updatetUser);
 
     return app.use('/', router);
 }
